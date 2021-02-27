baadf97845a684c43d80e2baf82e5bb60bfd951f
www42 <thomas.jaekel@gfn.de>
2021-02-27 13:20:25 +0100

    login script modified

--- tj/LOGIN.ps1
+++ tj/LOGIN.ps1
@@ -1,3 +1,11 @@
+# MSDN-Plattformen  Paul@az.training
+# Subscription ID:  ffcb38a5-8428-40c4-98b7-77013eac7ec5
+# Tenant ID:        819ebf55-0973-4703-b006-581a48f25961
+
+# Azure Pass        Paul@adatum6404.onmicrosoft.com
+# Subscription ID:  c46281f7-38b7-4e1d-ba22-140cd892cd07
+# Tenant ID:        89ee3811-46d2-4152-af70-352fcce4d323
+
 Login-AzAccount
 
 Get-AzSubscription | ft Name,Id,State
